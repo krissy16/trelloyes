15ac7fea11ba0dd3d55e1f88ade6ed8093c266f0
Krissy <krissysaenz@gmail.com>
2020-02-26 22:15:02 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './List.js';
+import './App.css'
 
-function App() {
+function App(props) {
+  const lists = props.store.lists.map(list=>{
+    const cards = list.cardIds.map(id => props.store.allCards[id]);
+    return <List key={list.id} header={list.header} cards={cards}/>;
+  });
   return (
-    <div className="App">
+    <main className='App'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trelloyes!</h1>
       </header>
-    </div>
+      <div className="App-list">
+        {lists}
+      </div>
+    </main>
   );
 }
 
